260e960d2fc53a2de32a36d0199b6282cc7bf450
rberberat <robin.berberat@corporatesoftware.ch>
2019-08-20 12:41:15 +0200

    Add additional parameters to create-pdf.ps1

--- scripts/pdf/create-pdf.ps1
+++ scripts/pdf/create-pdf.ps1
@@ -18,13 +18,24 @@ ForEach ($File in $Files){
   }
 }
 
-$ArgumentList = '-s -V geometry:margin=1in'
+$ArgumentList = ''
 $ArgumentList = $ArgumentList + (' -o ' + "`"$TargetPDF`"")
 $ArgumentList = $ArgumentList + (' -i ' + $mdFiles)
+
+$ArgumentList = $ArgumentList + (' -s')
+$ArgumentList = $ArgumentList + (' -f' + ' markdown_github')
+$ArgumentList = $ArgumentList + (' --include-in-header' + ' pagebreak.tex')
+$ArgumentList = $ArgumentList + (' --toc')
+$ArgumentList = $ArgumentList + (' --pdf-engine=xelatex')
+$ArgumentList = $ArgumentList + (' -V' + ' geometry:a4paper')
+$ArgumentList = $ArgumentList + (' -V' + ' geometry:margin=1in')
+$ArgumentList = $ArgumentList + (' -V' + ' linkcolor:blue')
+
 #$ArgumentList = $ArgumentList + (' --reference-doc=' + $DocPath + '\custom-reference.docx')
-#$ArgumentList = $ArgumentList + (' --toc')
 #$ArgumentList = $ArgumentList + (' --metadata-file=' + $DocPath + '\metadata.yml')
 
 Start-Process -Wait -NoNewWindow -FilePath $pathToPandoc -ArgumentList $ArgumentList
+
+Write-Host "Process finished"
 
 #Start-Process -Wait -NoNewWindow -FilePath "$Env:ProgramData\chocolatey\bin\pandoc.exe" -ArgumentList "-s -V geometry:margin=1in -o $TargetPDF $mdFiles"
